0a7b11d515d2fba19af616a50dbd48cf00e7cbaa
Clayton Voges <38054771+cvoges12@users.noreply.github.com>
2019-04-06 08:03:08 -0500

    ISO Config

---
+++ iso.nix
@@ -0,0 +1,12 @@
+{config, pkgs, ...}:
+
+{ 
+  imports = [
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+  ];
+
+  environment.systemPackages = with pkgs; [
+    git
+  ];
+}
